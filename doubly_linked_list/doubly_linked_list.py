7824b0ee5a2faf808f76f65377acf108386a9b71
Nicholas Mullen <nwmullen@gmail.com>
2020-07-22 22:32:21 -0400

    linkedlist, stack, queue, & doublylinkedlist

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -2,16 +2,21 @@
 Each ListNode holds a reference to its previous node
 as well as its next node in the List.
 """
+
+
 class ListNode:
     def __init__(self, value, prev=None, next=None):
         self.prev = prev
         self.value = value
         self.next = next
-            
+
+
 """
-Our doubly-linked list class. It holds references to 
+Our doubly-linked list class. It holds references to
 the list's head and tail nodes.
 """
+
+
 class DoublyLinkedList:
     def __init__(self, node=None):
         self.head = node
@@ -20,63 +25,153 @@ class DoublyLinkedList:
 
     def __len__(self):
         return self.length
-    
+
     """
-    Wraps the given value in a ListNode and inserts it 
-    as the new head of the list. Don't forget to handle 
+    Wraps the given value in a ListNode and inserts it
+    as the new head of the list. Don't forget to handle
     the old head node's previous pointer accordingly.
     """
     def add_to_head(self, value):
-        pass
-        
+        # List of 1 or more elements
+        if self.head:
+            old_head = self.head
+            new_head = ListNode(value, None, old_head)
+            old_head.prev = new_head
+            self.head = new_head
+        # Empty list
+        else:
+            new_head = ListNode(value)
+            self.head = new_head
+            self.tail = new_head
+
+        self.length += 1
+
     """
     Removes the List's current head node, making the
     current head's next node the new head of the List.
     Returns the value of the removed Node.
     """
     def remove_from_head(self):
-        pass
-            
+        # List of > 1 elements
+        if self.head and self.head.next:
+            old_head_val = self.head.value
+            self.head = self.head.next
+            self.head.prev = None
+            self.length -= 1
+            return old_head_val
+        # List of 1 element
+        elif self.head:
+            old_head_val = self.head.value
+            self.head = None
+            self.tail = None
+            self.length = 0
+            return old_head_val
+        # Empty list
+        else:
+            return None
+
     """
-    Wraps the given value in a ListNode and inserts it 
-    as the new tail of the list. Don't forget to handle 
+    Wraps the given value in a ListNode and inserts it
+    as the new tail of the list. Don't forget to handle
     the old tail node's next pointer accordingly.
     """
     def add_to_tail(self, value):
-        pass
-            
+        # List of 1 or more elements
+        if self.head:
+            new_tail = ListNode(value, self.tail)
+            self.tail.next = new_tail
+            self.tail = new_tail
+        # Empty list
+        else:
+            new_tail = ListNode(value)
+            self.head = new_tail
+            self.tail = new_tail
+
+        self.length += 1
+
     """
-    Removes the List's current tail node, making the 
+    Removes the List's current tail node, making the
     current tail's previous node the new tail of the List.
     Returns the value of the removed Node.
     """
     def remove_from_tail(self):
-        pass
-            
+        # List of > 1 elements
+        if self.head and self.head.next:
+            old_tail_val = self.tail.value
+            self.tail = self.tail.prev
+            self.tail.next = None
+            self.length -= 1
+            return old_tail_val
+        # List of 1 element
+        elif self.head:
+            old_tail_val = self.tail.value
+            self.head = None
+            self.tail = None
+            self.length = 0
+            return old_tail_val
+        # Empty list
+        else:
+            return None
+
     """
-    Removes the input node from its current spot in the 
+    Removes the input node from its current spot in the
     List and inserts it as the new head node of the List.
     """
     def move_to_front(self, node):
-        pass
-        
+        self.delete(node)
+        self.add_to_head(node.value)
+
     """
-    Removes the input node from its current spot in the 
+    Removes the input node from its current spot in the
     List and inserts it as the new tail node of the List.
     """
     def move_to_end(self, node):
-        pass
+        self.delete(node)
+        self.add_to_tail(node.value)
 
     """
-    Deletes the input node from the List, preserving the 
+    Deletes the input node from the List, preserving the
     order of the other elements of the List.
     """
     def delete(self, node):
-        pass
+        # List of > 1 elements
+        if self.head and self.head.next:
+            previous_node = node.prev
+            next_node = node.next
+            if previous_node:
+                previous_node.next = next_node
+            else:
+                self.head = next_node
+            if next_node:
+                next_node.prev = previous_node
+            else:
+                self.tail = previous_node
+            self.length -= 1
+        # List of 1 element
+        elif self.head is node:
+            self.head = None
+            self.tail = None
+            self.length = 0
+        # Empty list
+        else:
+            return None
 
     """
-    Finds and returns the maximum value of all the nodes 
+    Finds and returns the maximum value of all the nodes
     in the List.
     """
     def get_max(self):
-        pass
+        # Non-empty list
+        if self.head:
+            current_max = self.head.value
+            current_node = self.head
+            while current_node.next:
+                current_node = current_node.next
+                if current_node.value > current_max:
+                    current_max = current_node.value
+                else:
+                    pass
+            return current_max
+        # Empty list
+        else:
+            return None
--- queue/queue.py
+++ queue/queue.py
@@ -1,28 +1,75 @@
+import sys
+sys.path.append('../singly_linked_list')
+from singly_linked_list import LinkedList
 """
 A queue is a data structure whose primary purpose is to store and
-return elements in First In First Out order. 
+return elements in First In First Out order.
 
 1. Implement the Queue class using an array as the underlying storage structure.
    Make sure the Queue tests pass.
 2. Re-implement the Queue class, this time using the linked list implementation
    as the underlying storage structure.
    Make sure the Queue tests pass.
-3. What is the difference between using an array vs. a linked list when 
+3. What is the difference between using an array vs. a linked list when
    implementing a Queue?
-   
+
 Stretch: What if you could only use instances of your Stack class to implement the Queue?
          What would that look like? How many Stacks would you need? Try it!
 """
+
+
+#  class Queue:
+#      def __init__(self):
+#          self.size = 0
+#          self.storage = []
+#
+#      def __len__(self):
+#          return len(self.storage)
+#
+#      def enqueue(self, value):
+#          self.storage.append(value)
+#
+#      def dequeue(self):
+#          if self.storage:
+#              val = self.storage[0]
+#              self.storage = self.storage[1:]
+#              return val
+#          else:
+#              return
+
+
 class Queue:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
-    
+        self.storage = LinkedList()
+
     def __len__(self):
-        pass
+        head = self.storage.head
+        if head:
+            count = 1
+            next_node = head.get_next()
+            while next_node:
+                count += 1
+                next_node = next_node.get_next()
+            return count
+        else:
+            return 0
 
     def enqueue(self, value):
-        pass
+        self.storage.add_to_tail(value)
 
     def dequeue(self):
-        pass
+        # Directly manipulating the head and tail properties of the
+        # linkedlist feels like cheating...
+        head = self.storage.head
+        if head is None:
+            return
+        elif head is self.storage.tail:
+            val = head.get_value()
+            self.storage.head = None
+            self.storage.tail = None
+            return val
+        else:
+            val = head.get_value()
+            self.storage.head = head.get_next()
+            return val
